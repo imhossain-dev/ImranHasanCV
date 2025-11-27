cc034774b4e099e53a013cd28e596a916c4f6bc8
Imran Hasan <70254704+imhossain-dev@users.noreply.github.com>
2025-11-27 16:09:45 +0600

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,17 +1,23 @@
 <?php
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $to = "imranhasan49.rt@gmail.com";
-    $subject = "New message from your portfolio website";
-    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $message = htmlspecialchars($_POST["message"]);
 
-    if(mail($to, $subject, $body)) {
-        echo "Message sent";
+    $to = "imranhasan49.rt@gmail.com"; 
+    $subject = "New Message From Portfolio Website";
+    $body = "You received a new message from your website contact form.\n\n".
+            "Name: $name\n".
+            "Email: $email\n\n".
+            "Message:\n$message";
+
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "<h2 style='text-align:center;'>Message sent successfully! ✔</h2>";
     } else {
-        echo "Error sending message";
+        echo "<h2 style='text-align:center; color:red;'>Error sending message ❌</h2>";
     }
 }
 ?>
